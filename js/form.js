a4f7bd7dc40d472444d16bbae712b47c84b32c17
Bruno Rocha de Lima <brunorocha33@live.com>
2022-03-01 16:38:04 -0300

    Criando objeto paciente

--- js/form.js
+++ js/form.js
@@ -2,12 +2,11 @@ var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click", function (event) {
   event.preventDefault();
 
+  // extrair valores do formulário
   var form = document.querySelector("#form-adiciona");
-  var nome = form.nome.value;
-  var peso = form.peso.value;
-  var altura = form.altura.value;
-  var gordura = form.gordura.value;
+  extrairValoresFormulario(form);
 
+  // Cria a tr e td do paciente
   var pacienteTr = document.createElement("tr");
   var nomeTd = document.createElement("td");
   var pesoTd = document.createElement("td");
@@ -19,7 +18,7 @@ botaoAdicionar.addEventListener("click", function (event) {
   pesoTd.textContent = peso;
   alturaTd.textContent = altura;
   gorduraTd.textContent = gordura;
-  imcTd.textContent = calculaImc(peso, altura)
+  imcTd.textContent = calculaImc(peso, altura);
 
   pacienteTr.appendChild(nomeTd);
   pacienteTr.appendChild(pesoTd);
@@ -27,6 +26,21 @@ botaoAdicionar.addEventListener("click", function (event) {
   pacienteTr.appendChild(gorduraTd);
   pacienteTr.appendChild(imcTd);
 
+  // Adiciona o paciente na tabela
   tabelaPacientes = document.querySelector("#tabela-pacientes");
   tabelaPacientes.appendChild(pacienteTr);
 });
+
+
+function extrairValoresFormulario(form) {
+
+  var paciente = {
+    nome: form.nome.value,
+    peso: form.peso.value,
+    altura: form.altura.value,
+    gordura: form.gordura.value, //colocar virgula no final de cada propriedade do objeto, exceto no último
+    imc: calculaImc(form.peso.value, form.altura.value)
+  }
+
+  console.log(paciente);
+}
