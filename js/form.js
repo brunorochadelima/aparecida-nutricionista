9c2da239a842421f2fd60cfcb9255cc5b9ccd30d
Bruno Rocha de Lima <brunorocha33@live.com>
2022-03-02 22:43:42 -0300

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,38 +1,21 @@
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
-botaoAdicionar.addEventListener("click", function (event) {
-  event.preventDefault();
+botaoAdicionar.addEventListener("click", function(event){
+    event.preventDefault();
 
-  // extrair valores do formulário
-  var form = document.querySelector("#form-adiciona");
-  extrairValoresFormulario(form);
+    var form = document.querySelector("#form-adiciona");
+    // Extraindo informacoes do paciente do form
+    var paciente = obtemPacienteDoFormulario(form);
+    // Cria a tr e a td do paciente
+    var pacienteTr = montaTr(paciente);
 
-  // Cria a tr e td do paciente
-  var pacienteTr = document.createElement("tr");
-  var nomeTd = document.createElement("td");
-  var pesoTd = document.createElement("td");
-  var alturaTd = document.createElement("td");
-  var gorduraTd = document.createElement("td");
-  var imcTd = document.createElement("td");
+    var tabela = document.querySelector("#tabela-pacientes");
 
-  nomeTd.textContent = nome.value;
-  pesoTd.textContent = peso;
-  alturaTd.textContent = altura;
-  gorduraTd.textContent = gordura.value;
-  imcTd.textContent = calculaImc(peso, altura);
+    tabela.appendChild(pacienteTr);
 
-  pacienteTr.appendChild(nomeTd);
-  pacienteTr.appendChild(pesoTd);
-  pacienteTr.appendChild(alturaTd);
-  pacienteTr.appendChild(gorduraTd);
-  pacienteTr.appendChild(imcTd);
-
-  // Adiciona o paciente na tabela
-  tabelaPacientes = document.querySelector("#tabela-pacientes");
-  tabelaPacientes.appendChild(pacienteTr);
 });
 
 
-function extrairValoresFormulario(form) {
+function obtemPacienteDoFormulario(form) {
 
   var paciente = {
     nome: form.nome.value,
@@ -41,6 +24,29 @@ function extrairValoresFormulario(form) {
     gordura: form.gordura.value, //colocar virgula no final de cada propriedade do objeto, exceto no último
     imc: calculaImc(form.peso.value, form.altura.value)
   }
-
   console.log(paciente);
+}
+
+function montaTr(paciente) {
+  var pacienteTr = document.createElement("tr");
+
+  var nomeTd = document.createElement("td");
+  var pesoTd = document.createElement("td");
+  var alturaTd = document.createElement("td");
+  var gorduraTd = document.createElement("td");
+  var imcTd = document.createElement("td");
+
+  nomeTd.textContent = paciente.nome;
+  pesoTd.textContent = paciente.peso;
+  alturaTd.textContent = paciente.altura;
+  gorduraTd.textContent = paciente.gordura;
+  imcTd.textContent = paciente.imc;
+
+  pacienteTr.appendChild(nomeTd);
+  pacienteTr.appendChild(pesoTd);
+  pacienteTr.appendChild(alturaTd);
+  pacienteTr.appendChild(gorduraTd);
+  pacienteTr.appendChild(imcTd);
+
+  return pacienteTr;
 }
