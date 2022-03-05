b39775675c4049a41b7a4eeb9fc10b204d5f4468
Bruno Rocha de Lima <brunorocha33@live.com>
2022-03-05 12:41:59 -0300

    Refatorando formulário

--- js/form.js
+++ js/form.js
@@ -2,23 +2,43 @@ var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click", function (event) {
   event.preventDefault();
 
-  // extrair valores do formulário
   var form = document.querySelector("#form-adiciona");
-  extrairValoresFormulario(form)
+  // extrair valores do formulário
+  var paciente = extrairValoresFormulario(form);
 
+  //Montar TR paciente
+  var pacienteTr = montaTr(paciente);
 
+  var tabela = document.querySelector("#tabela-pacientes");
+  tabela.appendChild(pacienteTr);
+});
+
+function extrairValoresFormulario(form) {
+  var paciente = {
+    nome: form.nome.value,
+    peso: form.peso.value,
+    altura: form.altura.value,
+    gordura: form.gordura.value, //colocar virgula no final de cada propriedade do objeto, exceto no último
+    imc: calculaImc(form.peso.value, form.altura.value),
+  };
+  return paciente;
+}
+
+function montaTr(paciente){
   var pacienteTr = document.createElement("tr");
+  pacienteTr.classList.add("paciente");
+
   var nomeTd = document.createElement("td");
   var pesoTd = document.createElement("td");
   var alturaTd = document.createElement("td");
   var gorduraTd = document.createElement("td");
   var imcTd = document.createElement("td");
 
-  nomeTd.textContent = nome.value;
-  pesoTd.textContent = peso;
-  alturaTd.textContent = altura;
-  gorduraTd.textContent = gordura.value;
-  imcTd.textContent = calculaImc(peso, altura);
+  nomeTd.textContent = paciente.nome;
+  pesoTd.textContent = paciente.peso;
+  alturaTd.textContent = paciente.altura;
+  gorduraTd.textContent = paciente.gordura;
+  imcTd.textContent = paciente.imc;
 
   pacienteTr.appendChild(nomeTd);
   pacienteTr.appendChild(pesoTd);
@@ -26,20 +46,7 @@ botaoAdicionar.addEventListener("click", function (event) {
   pacienteTr.appendChild(gorduraTd);
   pacienteTr.appendChild(imcTd);
 
-  tabelaPacientes = document.querySelector("#tabela-pacientes");
-  tabelaPacientes.appendChild(pacienteTr);
-});
-
-
-function extrairValoresFormulario(form) {
-
-  var paciente = {
-    nome: form.nome.value,
-    peso: form.peso.value,
-    altura: form.altura.value,
-    gordura: form.gordura.value, //colocar virgula no final de cada propriedade do objeto, exceto no último
-    imc: calculaImc(form.peso.value, form.altura.value)
-  }
-
-  console.log(paciente);
+  return pacienteTr;
 }
+
+
