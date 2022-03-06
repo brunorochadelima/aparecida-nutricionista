1a16024066c5426fc793023d2a2f708915535b9c
Bruno Rocha de Lima <brunorocha33@live.com>
2022-03-06 12:16:21 -0300

    validando formulários

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -16,16 +16,16 @@ for (var index = 0; index < pacientes.length; index++) {
   var tdImc = paciente.querySelector(".info-imc");
 
   // Validação dos dados
-  var pesoValido = true;
-  var alturaValido = true;
+  var pesoValido = validaPeso(peso);
+  var alturaValido = validaAltura(altura);
 
-  if (peso <= 0 || peso >= 1000) {
+  if (!pesoValido) {
     pesoValido = false;
     tdImc.textContent = "Peso Inválido";
     paciente.classList.add("dado-invalido");
   }
 
-  if (altura <= 0 || altura >= 3.0) {
+  if (!validaAltura(altura)) {
     alturaValido = false;
     tdImc.textContent = "Altura Inválida";
     paciente.classList.add("dado-invalido");
@@ -40,4 +40,20 @@ function calculaImc(peso, altura) {
   var imc = 0;
   imc = peso / (altura * altura);
   return imc.toFixed(2);
+}
+
+function validaPeso(peso) {
+  if (peso >= 0 && peso <= 1000){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function validaAltura(altura) {
+  if (altura >= 0 && altura <= 3.00){
+    return true;
+  } else {
+    return false;
+  }
 }
